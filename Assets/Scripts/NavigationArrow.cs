51d255f3cacd2024d646d2e75490a0ef6dc60c3f
SedenCanpolat <sedencanpolat@gmail.com>
2024-11-15 15:40:42 +0300

    little debug

--- Assets/Scripts/NavigationArrow.cs
+++ Assets/Scripts/NavigationArrow.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class NavigationArrow : MonoBehaviour
 {
     [SerializeField] private GameObject _arrow;
     private float _targetLatitude;
     private float _targetLongitude;
+    public TextMeshProUGUI textMessage;
+
 
     private void Start() {
         SetDestination(38.45180248754061f, 27.201164958903657f); // for debug
@@ -14,6 +18,9 @@ public class NavigationArrow : MonoBehaviour
     private void _rotateArrow(){
         float angle = Gyroscope.FindAngleToTarget(_targetLatitude, _targetLongitude);
         _arrow.transform.localRotation = Quaternion.Euler(0,0,angle);
+
+        textMessage.text = $" {angle}";
+
     }
 
     public void SetDestination(float targetLatitude, float targetLongitude){
