8c8bf98999e1df6f2fa4141ee4c271f406a1eb8e
SedenCanpolat <sedencanpolat@gmail.com>
2024-12-03 23:02:52 +0300

    basic notification system

---
+++ Assets/Scripts/Notifications/AndroidNotification.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Notifications.Android;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class AndroidNotification : MonoBehaviour
+{
+   
+    public void RequestAuthorization(){
+        if(!Permission.HasUserAuthorizedPermission("android.permission.POST_NOTIFICATIONS")){
+            Permission.RequestUserPermission("android.permission.POST_NOTIFICATIONS");
+        }
+    }
+
+    public void RegisterNotificationChannel(){
+        var channel = new AndroidNotificationChannel{
+            Id = "default_channel",
+            Name = "Default Channel",
+            Importance = Importance.Default,
+            Description = "Discovered the Location"
+        };
+
+        AndroidNotificationCenter.RegisterNotificationChannel(channel);
+    }
+
+    public void SendNotification(string title, string text){
+        var notification = new Unity.Notifications.Android.AndroidNotification{
+            Text = text,
+            Title = title
+            //SmallIcon = "icon_0"
+        };
+        
+        AndroidNotificationCenter.SendNotification(notification, "default_channel");
+    }
+   
+}
---
+++ Assets/Scripts/Notifications/NotificationManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+using Unity.Notifications.Android;
+
+
+public class NotificationManager : MonoBehaviour
+{
+    [SerializeField] AndroidNotification androidNotification;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        androidNotification.RequestAuthorization();
+        androidNotification.RegisterNotificationChannel();
+    }
+
+    private void OnApplicationFocus(bool focusStatus) {
+        if(focusStatus == true){
+            androidNotification.SendNotification("New Discovery", "You discovered a new location!");
+        }
+    }
+}
