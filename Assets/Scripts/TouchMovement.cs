d11a4a41ac5162b35523f752d7e033adf28048c5
SedenCanpolat <sedencanpolat@gmail.com>
2024-12-11 13:26:36 +0300

    changes on speed but its useless

--- Assets/Scripts/TouchMovement.cs
+++ Assets/Scripts/TouchMovement.cs
@@ -7,25 +7,28 @@ using UnityEngine.UIElements;
 public class TouchMovement : MonoBehaviour
 {
 
-     private Vector2 startTouchPosition;
+    private Vector2 startTouchPosition;
     private Vector2 currentTouchPosition;
     private bool isTouching = false;
 
-    public float movementSpeed = 0.9f;
-    public float sensitivity = 0.001f;
+    public float movementSpeed = 1000f;
+    public float sensitivity = 100f;
 
     void Update(){
         if (Input.touchCount > 0){
             Touch touch = Input.GetTouch(0);
-            if(touch.phase == TouchPhase.Began) startTouchPosition = touch.position;
-            if(touch.phase == TouchPhase.Moved) {
+            if(touch.phase == TouchPhase.Began){
+                isTouching = true;
+                startTouchPosition = touch.position;
+            }
+            if(touch.phase == TouchPhase.Moved && isTouching){ 
                 currentTouchPosition = touch.position;
                 Vector2 deltaPosition = (currentTouchPosition - startTouchPosition) * sensitivity;
                 transform.Translate(new Vector3(deltaPosition.x, 0, deltaPosition.y) * movementSpeed, Space.World);
                 startTouchPosition = currentTouchPosition;
                 Debug.Log($"Delta Position: {deltaPosition}");
 
-             }
+            }
         }
     }
 }
