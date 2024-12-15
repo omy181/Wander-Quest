751b4ddd2c4eec0a835f1d3d54fde99db971e6fc
SedenCanpolat <sedencanpolat@gmail.com>
2024-12-15 22:00:18 +0300

    it happened i guess :D

---
+++ Assets/Scripts/PerspectivePan.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerspectivePan : MonoBehaviour
+{
+    private Vector3 _touchStart; 
+    public Camera cam; 
+    public float groundZ = 0f;
+
+    void Update(){
+        if (Input.GetMouseButtonDown(0)){
+            _touchStart = _getWorldPosition(groundZ);
+        }
+
+        if (Input.GetMouseButton(0)){
+            Vector3 direction = _touchStart - _getWorldPosition(groundZ);
+            cam.transform.position += new Vector3(direction.x, direction.y, 0); 
+        }
+    }
+
+    private Vector3 _getWorldPosition(float zLevel){
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        Plane ground = new Plane(Vector3.forward, new Vector3(0, 0, zLevel));
+        if (ground.Raycast(ray, out float distance)){
+            return ray.GetPoint(distance);
+        }
+        return Vector3.zero;
+    }
+}
